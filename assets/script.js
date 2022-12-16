e3cb0273b4ac331d534c83ae7b2cbf198716b40a
[Jacob Kouns] <[jacob.kouns01@gmail.com]>
2022-12-16 15:43:28 -0600

    adding javascript

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+
+var alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*_-+=";
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
