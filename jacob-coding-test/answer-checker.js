b1cbe104e1ba8c4741441fbe34a9e1ecc5af2dc2
[Jacob Kouns] <[jacob.kouns01@gmail.com]>
2023-01-01 15:45:20 -0600

    corrected some issues with javascript and css

--- jacob-coding-test/answer-checker.js
+++ jacob-coding-test/answer-checker.js
@@ -5,6 +5,18 @@ const Q1 = document.querySelector("questionOne");
 const Q2 = document.querySelector("questionTwo");
 const Q3 = document.querySelector("questionThree");
 const Q4 = document.querySelector("questionFour");
+const B1 = document.getElementById("block-1");
+const B2 = document.getElementById("block-2");
+const B3 = document.getElementById("block-3");
+const B4 = document.getElementById("block-4");
+const B5 = document.getElementById("block-5");
+const B6 = document.getElementById("block-6");
+const B7 = document.getElementById("block-7");
+const B8 = document.getElementById("block-8");
+const B9 = document.getElementById("block-9");
+const B10 = document.getElementById("block-10");
+const B11 = document.getElementById("block-11");
+const B12 = document.getElementById("block-12");
 
 // if answer is correct, add 2 points. else wrong, add 0 points //
 
@@ -13,3 +25,4 @@ const Q4 = document.querySelector("questionFour");
 // need to figure out how to show only one question at a time. once answered it will direct you to the next answer//
 
 // track every correct answer and save it in local with name option
+
