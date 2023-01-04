b82d9e7fde2a48fa64211ddc4d90b1208e269e67
[Jacob Kouns] <[jacob.kouns01@gmail.com]>
2023-01-03 19:28:55 -0600

    corrected javaScript to look a lot cleaner

--- jacob-coding-test/answer-checker.js
+++ jacob-coding-test/answer-checker.js
@@ -1,22 +1,42 @@
 //Start
-const START_SECTION = document.getElementById("start");
-const START_BTN = document.getElementById("start-button");
-const Q1 = document.querySelector("questionOne");
-const Q2 = document.querySelector("questionTwo");
-const Q3 = document.querySelector("questionThree");
-const Q4 = document.querySelector("questionFour");
-const B1 = document.getElementById("block-1");
-const B2 = document.getElementById("block-2");
-const B3 = document.getElementById("block-3");
-const B4 = document.getElementById("block-4");
-const B5 = document.getElementById("block-5");
-const B6 = document.getElementById("block-6");
-const B7 = document.getElementById("block-7");
-const B8 = document.getElementById("block-8");
-const B9 = document.getElementById("block-9");
-const B10 = document.getElementById("block-10");
-const B11 = document.getElementById("block-11");
-const B12 = document.getElementById("block-12");
+var questions = [
+    {
+        question: "What is a variable?",
+        optionA: "Containers for storing data",
+        optionB: "Are placeholders",
+        optionC: "A number used in math",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "What is not a variable type?",
+        optionA: "String",
+        optionB: "Line",
+        optionC: "Number",
+        correctOption: "optionB"
+    },
+
+    {
+        question:"What is an array?",
+        optionA:"Store multiple values in one variable",
+        optionB: "Same as a method",
+        optionC:"Used to create a list",
+        correctOption: "optionA"
+    },
+
+    {
+        question:"How does JavaScript interact with HTML?",
+        optionA:"It doesn't interact",
+        optionB:"It adds coloring to HTML",
+        optionC:"Enhances the pages capabilities",
+        correctOption: "optionC"
+    }
+]
+
+
+
+
+
 
 // if answer is correct, add 2 points. else wrong, add 0 points //
 
